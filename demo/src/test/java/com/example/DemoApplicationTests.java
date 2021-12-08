2b9a9de3be657c0127cf3e3ba1dcddcb3a006cae
Lu Luo <lu.luo@sap.com>
2021-12-08 14:40:08 -0800

    Fix build

--- demo/src/test/java/com/example/DemoApplicationTests.java
+++ demo/src/test/java/com/example/DemoApplicationTests.java
@@ -1,39 +1,35 @@
 package com.example;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class DemoApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Autowired
-	ConfigurableEnvironment environment;
+    @Autowired
+    ConfigurableEnvironment environment;
 
-	@Test
-	public void contextLoads() {
-		this.environment.getProperty("spring.application.name");
-	}
+    @Test
+    public void contextLoads() {
+        this.environment.getProperty("spring.application.name");
+    }
 
-	@Test
-	public void resource() throws Exception {
-		this.mockMvc.perform(get("/greeting"))
-				.andExpect(content().string(containsString("Hello World!")));
-	}
+    @Test
+    public void resource() throws Exception {
+        this.mockMvc.perform(get("/greeting"))
+                .andExpect(content().string(containsString("Hello World!")));
+    }
 
 }
